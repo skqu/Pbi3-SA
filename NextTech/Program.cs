24a91b2f19f55f46b837ee91d165c29fefa4ae96
stefanquvang <skqu@iba.dk>
2024-11-04 10:13:06 +0100

    Create simple API

--- NextTech/Program.cs
+++ NextTech/Program.cs
@@ -1,16 +1,59 @@
-using System.ComponentModel;
-using System.Globalization;
-using System.Runtime.CompilerServices;
+var builder = WebApplication.CreateBuilder(args);
 
-class Program
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Add CORS policy
+builder.Services.AddCors(options =>
 {
-    static void Main(string[] args)
-    {
-        var logger = new Logger();
+    options.AddPolicy("AllowAll",
+        policy => policy.AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader());
+});
 
-        var calculater = new Calc();
-        logger.Info(calculater.add(2,2).ToString());
-        
-    }
+var app = builder.Build();
 
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+// Enable CORS globally
+app.UseCors("AllowAll");
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+var forecasts = new List<WeatherForecast>(); // In-memory storage for weather data
+
+app.MapGet("/weatherforecast", () =>
+{
+    
+    return forecasts;
+})
+.WithName("GetWeatherForecast")
+.WithOpenApi();
+
+app.MapPost("/weatherforecast/add", (WeatherForecast newForecast) =>
+{
+    forecasts.Add(newForecast);
+    return Results.Created("/weatherforecast/add", newForecast);
+})
+.WithName("AddWeatherForecast")
+.WithOpenApi();
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 }
